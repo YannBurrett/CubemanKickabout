a0517404a535f2f44e9846d3a705983954a30417
Yann Burrett <yann.let@gmail.com>
2018-06-18 17:20:28 +0100

    Changed background score effect to Tween

--- Scripts/Level.gd
+++ Scripts/Level.gd
@@ -3,6 +3,11 @@ extends Spatial
 signal Goal1 
 signal Goal2
 
+var black = Color(0,0,0)
+var purple = Color(0.25,0,0.25)
+var green = Color(0,0.25,0)
+var colour_target = black
+
 
 func _ready():
 	reset()
@@ -10,7 +15,7 @@ func _ready():
 
 
 func reset(): # move the players and balls to the starting positions
-	$WorldEnvironment.environment.background_color = Color(0,0,0)
+	$WorldEnvironment.environment.background_color = black
 	$Ball.translation = $Spawns/BallSpawn.translation
 	$Player1.translation = $Spawns/Player1Spawn.translation
 	$Player2.translation = $Spawns/Player2Spawn.translation
@@ -23,8 +28,7 @@ func _on_Goal1Detect_body_entered(body):
 		emit_signal("Goal1")
 		$Player1.can_move = false
 		$Player2.can_move = false
-		$WorldEnvironment.environment.background_color = Color(0.25,0,0.25)
-		goal()
+		goal(purple)
 
 
 func _on_Goal2Detect_body_entered(body):
@@ -32,13 +36,13 @@ func _on_Goal2Detect_body_entered(body):
 		emit_signal("Goal2")
 		$Player1.can_move = false
 		$Player2.can_move = false
-		$WorldEnvironment.environment.background_color = Color(0,0.25,0)
-		goal()
+		goal(green)
 
 
-func goal():
+func goal(colour):
 	$Timer.start()
-
+	$WorldEnvironment/Tween.interpolate_property($WorldEnvironment.environment,"background_color", black, colour, 0.5, Tween.TRANS_SINE, Tween.EASE_IN )
+	$WorldEnvironment/Tween.start()
 
 func _on_Timer_timeout():
 	reset()
